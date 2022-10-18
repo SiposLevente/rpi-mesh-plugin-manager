e303aad938b14c35360ef349ce2459cd37b233bf
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-18 15:12:01 +0200

    fixed build.sh bugs

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-
-cargo_status=`ls /home/$USER/.cargo/bin/ | grep "cargo$"`
+cargo_status="`ls /home/$USER/.cargo/bin/ | grep "cargo$"`"
 if [[ $cargo_status == "" ]];then
     echo "Cargo is not installed!"
     echo "Please run: 'curl https://sh.rustup.rs -sSf | sh'"
     exit 1
 fi
 
-cargo build --release
+libssl_is_installed="`dpkg -l | grep libssl-dev`"
+if [[ $libssl_is_installed == "" ]];then
+    echo "Libssl-dev is not installed! Installing..."
+    sudo apt install -y libssl-dev
+    if [ $? == 0 ];then
+        successful=1
+    fi
+else
+    successful=1
+fi
+
+if [ $successful == 1 ];then
+    cargo build --release
+fi
