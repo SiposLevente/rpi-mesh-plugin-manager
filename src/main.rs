0dfadb1429b110e17baa2dad8ca575c01f8e6edc
SiposLevente <sipi.levente@gmail.com>
2022-11-04 14:29:50 +0100

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,20 @@ use std::env::args;
 use plugin_manager::PluginManager;
 
 fn main() {
-    let mut plug_manager = PluginManager::new();
-    plug_manager.cache_repos();
     match args().nth(1) {
-        Some(first_arg) => match first_arg.as_str() {
-            "install" => install(&mut plug_manager),
-            "update" => update(&mut plug_manager),
-            "upgrade" => upgrade(&plug_manager),
-            "uninstall" => uninstall(&plug_manager),
-            "list" => list(&plug_manager),
+        Some(first_arg) => {
+            let mut plug_manager = PluginManager::new();
+            plug_manager.cache_repos();
+            match first_arg.as_str() {
+                "install" => install(&mut plug_manager),
+                "update" => update(&mut plug_manager),
+                "upgrade" => upgrade(&plug_manager),
+                "uninstall" => uninstall(&plug_manager),
+                "list" => list(&plug_manager),
 
-            _ => print_help(),
-        },
+                _ => print_help(),
+            }
+        }
         None => print_help(),
     }
 }
